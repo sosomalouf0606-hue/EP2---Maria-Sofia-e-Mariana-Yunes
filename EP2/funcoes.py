91821234020927aea75229253da0c254ac4d0a30
Maria Sofia Malouf <sosomalouf0606@gmail.com>
2026-04-27 17:55:11 -0300

    Exercício 2

--- EP2/funcoes.py
+++ EP2/funcoes.py
@@ -6,4 +6,8 @@ def rolar_dados (n):
         l.append(a)
     return l
 
+def guardar_dado (dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+    del dados_rolados [dado_para_guardar]
+    return [dados_rolados, dados_no_estoque]
 
