5c2c854a2fafe597a563c89b3b4b749f370e5712
Mariana <mariguimayunes@gmail.com>
2026-04-28 14:39:21 -0300

    fiz o ex 3

--- EP2/funcoes.py
+++ EP2/funcoes.py
@@ -11,4 +11,16 @@ def guardar_dado (dados_rolados, dados_no_estoque, dado_para_guardar):
     del dados_rolados [dado_para_guardar]
     return [dados_rolados, dados_no_estoque]
 
-# Alteração
+def remover_dado(dados_rolados, dados_no_estoque, n): #n= índice do dado a ser removido do estoque
+    lista_dados=[]
+    dado_removido=dados_no_estoque[n]
+    novo_estoque=[]
+    i=0
+    while i<len(dados_no_estoque):
+        if i!=n:
+            novo_estoque.append(dados_no_estoque[i])
+    dados_rolados.append(dado_removido)
+    dados_no_estoque=dados_no_estoque-dado_removido
+    lista_dados.append(dados_rolados)
+    lista_dados.append(novo_estoque)
+    return lista_dados
